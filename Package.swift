476618f455f533f9af84b862166ada58f0ac72c7
Muhammad Rajab <m.rajab.priharsanto@gmail.com>
2025-06-02 15:44:06 +0700

    change package name back to MyPackage

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "TimerModule",
+    name: "MyPackage",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
