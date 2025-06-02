6d29fc42a4f5c841791bed0c667be4fba1b3b554
Muhammad Rajab <m.rajab.priharsanto@gmail.com>
2025-06-02 15:25:01 +0700

    Initial Commit

---
+++ Sources/MyPackage/TimeAndDateFormat.swift
@@ -0,0 +1,125 @@
+//
+//  TimeAndDateFormat.swift
+//
+//  Created by Priharsanto, Muhammad Rajab on 15/05/25.
+//
+
+import Foundation
+
+enum DateFormat {
+    ///date format: dd-MM-yyyy HH:mm:ss
+    case fullDateWithTime
+    
+    ///date format: dd-MM-yyyy HH:mm
+    case fullDateWithHourAndMinute
+    
+    ///date format: dd-MM-yyyy
+    case fullDate
+    
+    ///date format: dd MMMM yyyy
+    case fullDateComplete
+    
+    ///date format: EEEE dd MMMM yyyy
+    case fullDateCompleteWithWeekday
+    
+    ///date format: yyyy
+    case year
+    
+    ///date format: dd MMM
+    case dateAndMonth
+    
+    ///date format: dd MMM yyyy
+    case dateMonthAndYear
+    
+    ///date format: yyyy-MM-dd'T'HH:mm:ss.SSSZ
+    case fullDateWithTimezone
+    
+    ///date format: yyyy-MM-dd'T'HH:mm:ssZ
+    case fullDateWithTimezoneWithoutSSS
+    
+    ///date format: yyyy-MM-dd'T'HH:mm:ss.SSZ
+    case fullDateWithTimezoneWithSS
+    
+    ///date format: yyyyMMdd_HHmmss
+    case uploadImageFileName
+    
+    case custom(format: String)
+}
+
+func dateFromString(string: String, format: DateFormat = .fullDate) -> Date {
+    let formatter = DateFormatter()
+    formatter.locale = Locale(identifier: "id_ID")
+    formatter.timeZone = TimeZone.autoupdatingCurrent
+    switch format {
+    case .fullDateWithTime:
+        formatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
+    case .fullDateWithHourAndMinute:
+        formatter.dateFormat = "dd-MM-yyyy HH:mm"
+    case .fullDate:
+        formatter.dateFormat = "dd-MM-yyyy"
+    case .fullDateComplete:
+        formatter.dateFormat = "dd MMMM yyyy"
+    case .fullDateCompleteWithWeekday:
+        formatter.dateFormat = "EEEE dd MMMM yyyy"
+    case .year:
+        formatter.dateFormat = "yyyy"
+    case .dateAndMonth:
+        formatter.dateFormat = "dd MMM"
+    case .dateMonthAndYear:
+        formatter.dateFormat = "dd MMM yyyy"
+    case .fullDateWithTimezone:
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    case .fullDateWithTimezoneWithoutSSS:
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+    case .fullDateWithTimezoneWithSS:
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSZ"
+    case .uploadImageFileName:
+        formatter.dateFormat = "yyyyMMdd_HHmmss"
+    case .custom(let format):
+        formatter.dateFormat = format
+    }
+    return formatter.date(from: string) ?? Date()
+}
+
+func getCurrentDateTime(date: Date = Date(), format: DateFormat = .fullDateWithTime) -> String {
+    let formatter = DateFormatter()
+    
+    formatter.locale = Locale(identifier: "id_ID")
+    switch format {
+    case .fullDateWithTime:
+        formatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
+    case .fullDateWithHourAndMinute:
+        formatter.dateFormat = "dd-MM-yyyy HH:mm"
+    case .fullDate:
+        formatter.dateFormat = "dd-MM-yyyy"
+    case .fullDateComplete:
+        formatter.dateFormat = "dd MMMM yyyy"
+    case .fullDateCompleteWithWeekday:
+        formatter.dateFormat = "EEEE dd MMMM yyyy"
+    case .year:
+        formatter.dateFormat = "yyyy"
+    case .dateAndMonth:
+        formatter.dateFormat = "dd MMM"
+    case .dateMonthAndYear:
+        formatter.dateFormat = "dd MMM yyyy"
+    case .fullDateWithTimezone:
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    case .fullDateWithTimezoneWithoutSSS:
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+    case .fullDateWithTimezoneWithSS:
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSZ"
+    case .uploadImageFileName:
+        formatter.dateFormat = "yyyyMMdd_HHmmss"
+    case .custom(let format):
+        formatter.dateFormat = format
+    }
+    return formatter.string(from: date)
+}
+
+func dateFromApiToString(string: String,
+                         inputFormat: DateFormat = .fullDate,
+                         outputFormat: DateFormat = .fullDateWithTime) -> String {
+    let inputDate = dateFromString(string: string, format: inputFormat)
+    let outputDate = getCurrentDateTime(date: inputDate, format: outputFormat)
+    return outputDate
+}
---
+++ Sources/MyPackage/TimerView.swift
@@ -0,0 +1,101 @@
+//
+//  TimerView.swift
+//
+//  Created by Priharsanto, Muhammad Rajab on 15/05/25.
+//
+
+import Combine
+
+#if canImport(SwiftUI)
+import SwiftUI
+#endif
+
+#Preview {
+    let vm = TimerViewModel(generatedDate: "2025-06-02T19:31:00+07:00")
+    TimerView(viewModel: vm)
+}
+
+class TimerViewModel: ObservableObject {
+    enum ViewState {
+        case success
+        case loading
+        case error
+    }
+
+    private var timer: Cancellable?
+    private var timeRemaining: TimeInterval
+    @Published var viewState: ViewState
+    @Published var generatedDate: String
+    
+    init(timer: Cancellable? = nil, timeRemaining: TimeInterval = 10, viewState: ViewState = .success, generatedDate: String = "") {
+        self.timer = timer
+        self.timeRemaining = timeRemaining
+        self.viewState = viewState
+        self.generatedDate = generatedDate
+        
+        self.startTimer(time: generatedDate)
+    }
+    
+    func startTimer(time: String) {
+        let dates = dateFromString(string: time,
+                                   format: .fullDateWithTimezoneWithoutSSS)
+        let timeIntervalSeconds = dates.timeIntervalSince(Date())
+        timeRemaining = timeIntervalSeconds
+        
+        timer = Timer.publish(every: 1, on: .current, in: .default)
+            .autoconnect()
+            .sink { _ in } receiveValue: { [weak self] _ in
+                guard let self = self else { return }
+                if timeRemaining > 2 {
+                    timeRemaining -= 1
+                    let currentDate = Date(timeIntervalSince1970: timeRemaining)
+                    let calendar = Calendar.current
+                    let minutes = calendar.component(.minute, from: currentDate)
+                    let seconds = calendar.component(.second, from: currentDate)
+                    generatedDate = String.init(format: "%02d:%02d", minutes, seconds)
+                } else {
+                    timer?.cancel()
+                    viewState = .error
+                }
+            }
+    }
+}
+
+struct TimerView: View {
+    @ObservedObject var viewModel: TimerViewModel
+    @State var time: String = "00:00"
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(viewModel.viewState == .success ? "Kadaluwarsa dalam" : "Waktu habis")
+//                    .font(.style(MPFonts.smallMedium))
+                Text(viewModel.viewState == .success ? time : "")
+//                    .font(.style(MPFonts.smallBold))
+            }
+            .padding(.top, 8)
+        }
+        .valueChanged(value: viewModel.generatedDate, onChange: {
+            time = $0
+        })
+    }
+}
+
+//extension Font {
+//    static func style(_ style: UIFont?) -> Font {
+//        return Font.custom(style?.fontName ?? MPFonts.defaultFontFamily(), size: style?.pointSize ?? MPFonts.defaultFontSize())
+//    }
+//}
+
+extension View {
+    /// A backwards compatible wrapper for iOS 14 `onChange`
+    @ViewBuilder func valueChanged<T: Equatable>(value: T, onChange: @escaping (T) -> Void) -> some View {
+        if #available(iOS 14.0, *) {
+            self.onChange(of: value, perform: onChange)
+        } else {
+            self.onReceive(Just(value)) { (value) in
+                onChange(value)
+            }
+        }
+    }
+}
